8008b8a144fadd84112b57103e6cc54ac2c9ec0c
zq99299 <99299684@qq.com>
2022-07-18 11:28:00 +0800

    NIO 例子的正确写法

--- netty-test/src/main/java/cn/mrcode/study/netty/test/nio/TimeClient.java
+++ netty-test/src/main/java/cn/mrcode/study/netty/test/nio/TimeClient.java
@@ -6,6 +6,7 @@ import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
 import java.sql.Time;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
@@ -15,53 +16,52 @@ import java.util.concurrent.TimeUnit;
  * @date 2022/7/17 18:50
  */
 public class TimeClient {
-    public static void main(String[] args) throws IOException, InterruptedException {
-        int port = 8086;
-        String host = "localhost";
-        Selector selector = Selector.open();
-        SocketChannel channel = SocketChannel.open();
-        //设置非阻塞
-        channel.configureBlocking(false);
-        // 设置关注连接事件，并且注册到选择器中
-        channel.register(selector, SelectionKey.OP_CONNECT);
-        // 发起链接
-        channel.connect(new InetSocketAddress(host, port));
+  public static void main(String[] args) throws IOException, InterruptedException {
+    int port = 8086;
+    String host = "localhost";
+    Selector selector = Selector.open();
+    SocketChannel channel = SocketChannel.open();
+    //设置非阻塞
+    channel.configureBlocking(false);
+    // 设置关注连接事件，并且注册到选择器中
+    channel.register(selector, SelectionKey.OP_CONNECT);
+    // 发起链接
+    channel.connect(new InetSocketAddress(host, port));
 
-        ByteBuffer buffer = ByteBuffer.allocate(1024);
-        while (true) {
-            selector.select();
-            Set<SelectionKey> keys = selector.selectedKeys();
-            for (SelectionKey key : keys) {
-                if (key.isConnectable()) {
-                    channel.finishConnect();
-                    // 链接成功，发送一个问候消息
-                    channel.write(ByteBuffer.wrap("你好".getBytes()));
-                    // 然后关注读写事件
-                    key.interestOps(SelectionKey.OP_READ | SelectionKey.OP_WRITE);
-                } else if (key.isReadable() && key.isValid()) {
-                    int readBytes = channel.read(buffer);
-                    if (readBytes == -1) {
-                        channel.close();
-                        key.cancel();
-                        System.out.println("客户端已关闭");
-                    } else {
-                        // 将 byteBuffer 的 position 和 limit 设置为 0，以便读取数据
-                        buffer.flip();
-                        // 读取数据
-                        byte[] bytes = new byte[buffer.remaining()];
-                        buffer.get(bytes);
-                        String body = new String(bytes, "UTF-8");
-                        System.out.println("收到信息：" + body);
-                        buffer.clear();
+    ByteBuffer buffer = ByteBuffer.allocate(1024);
+    while (true) {
+      selector.select();
+      Set<SelectionKey> keys = selector.selectedKeys();
+      for (SelectionKey key : keys) {
+        if (key.isConnectable()) {
+          channel.finishConnect();
+          // 链接成功，发送一个问候消息
+          channel.write(ByteBuffer.wrap("你好".getBytes(StandardCharsets.UTF_8)));
+          // 然后关注读事件
+          key.interestOps(SelectionKey.OP_READ);
+        } else if (key.isReadable()) {
+          // 假定每次都读取到了整包数据，不考虑半包，直接清空 buffer，重新读取
+          buffer.clear();
+          int readBytes = channel.read(buffer);
+          if (readBytes == -1) {
+            channel.close();
+            key.cancel();
+            System.out.println("客户端已关闭");
+          } else {
+            buffer.flip();
+            byte[] bytes = new byte[buffer.remaining()];
+            buffer.get(bytes);
+            String body = new String(bytes, StandardCharsets.UTF_8);
+            System.out.println("收到信息：" + body);
 
-                        channel.write(ByteBuffer.wrap("x".getBytes()));
-                        key.interestOps(key.interestOps() | SelectionKey.OP_WRITE);
-                        TimeUnit.SECONDS.sleep(5);
-                    }
-                }
-            }
-            // 处理万抽后一定要清空这次的事件，否则下次还会重复消费当次事件
-            keys.clear();
+            // 发送一条数据
+            channel.write(ByteBuffer.wrap("x".getBytes(StandardCharsets.UTF_8)));
+            TimeUnit.SECONDS.sleep(5);
+          }
         }
+      }
+      // 处理完成后，要删除当次的 keys，否词下次还会继续消费到
+      keys.clear();
     }
+  }
 }
--- netty-test/src/main/java/cn/mrcode/study/netty/test/nio/TimeServer.java
+++ netty-test/src/main/java/cn/mrcode/study/netty/test/nio/TimeServer.java
@@ -7,68 +7,97 @@ import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
+import java.util.Queue;
 import java.util.Set;
-import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ArrayBlockingQueue;
+
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.lang.Console;
 
 /**
  * @author mrcode
  * @date 2022/7/17 16:50
  */
 public class TimeServer {
-    public static void main(String[] args) throws IOException {
-        int port = 8086;
+  public static void main(String[] args) throws IOException {
+    int port = 8086;
 
-        // 初始化选择器
-        Selector selector = Selector.open();
-        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
-        serverSocketChannel.bind(new InetSocketAddress(port));
-        // 设置非阻塞
-        serverSocketChannel.configureBlocking(false);
-        // 注册自己感兴趣的事件，这里只对接受连接的事件感兴趣
-        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
-        while (true) {
-            // 使用阻塞的选择操作，有事件才会返回，否则阻塞
-            selector.select();
-            Set<SelectionKey> keys = selector.selectedKeys();
-            for (SelectionKey key : keys) {
-                if (key.isAcceptable()) {
-                    // 创建链接
-                    SocketChannel channel = serverSocketChannel.accept();
-                    // 设置非阻塞
-                    channel.configureBlocking(false);
-                    // 注册读写事件 并设置一个 byteBuffer 到 channel 对应的选择器中
-                    // 每个 channel 分配一个 byteBuffer 用来读写
-                    channel.register(selector, SelectionKey.OP_READ, ByteBuffer.allocate(1024));
-                } else if (key.isReadable() && key.isValid()) {
-                    // 读取数据
-                    SocketChannel channel = (SocketChannel) key.channel();
-                    ByteBuffer buffer = (ByteBuffer) key.attachment();
-                    int readBytes = channel.read(buffer);
-                    if (readBytes == -1) {
-                        channel.close();
-                        key.cancel();
-                        System.out.println("客户端已关闭");
-                    } else {
-                        // 将 byteBuffer 的 position 和 limit 设置为 0，以便读取数据
-                        buffer.flip();
-                        // 读取数据
-                        byte[] bytes = new byte[buffer.remaining()];
-                        buffer.get(bytes);
-                        String body = new String(bytes, "UTF-8");
-                        System.out.println("收到信息：" + body);
-
-                        buffer.clear();
-                        String currentTime = "当前时间：" + new Date();
-                        System.out.println("发送当前时间");
-                        buffer.put(currentTime.getBytes());
-                        buffer.flip();
-                        channel.write(buffer);
-                        key.interestOps(SelectionKey.OP_READ);
-                    }
-                }
-            }
-            keys.clear();
+    // 初始化选择器
+    Selector selector = Selector.open();
+    ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+    serverSocketChannel.bind(new InetSocketAddress(port));
+    Console.log("服务启动成功，监听端口 {}", port);
+    // 设置非阻塞
+    serverSocketChannel.configureBlocking(false);
+    // 注册自己感兴趣的事件，这里只对接受连接的事件感兴趣
+    serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+    while (true) {
+      // 使用阻塞的选择操作，有事件才会返回，否则阻塞
+      selector.select();
+      Set<SelectionKey> keys = selector.selectedKeys();
+      for (SelectionKey key : keys) {
+        if (key.isAcceptable()) {
+          // 创建链接
+          SocketChannel channel = serverSocketChannel.accept();
+          // 设置非阻塞
+          channel.configureBlocking(false);
+          // 注册读写事件, 并且为每个 channel 注册一个处理程序
+          channel.register(selector, SelectionKey.OP_READ | SelectionKey.OP_WRITE, new Handler(channel));
+        } else if (key.isReadable()) {
+          ((Handler) key.attachment()).read(key);
+        } else if (key.isWritable()) {
+          ((Handler) key.attachment()).checkWrite();
         }
+      }
+      keys.clear();
     }
+  }
+
+  /**
+   * 为每个 连接 处理的事件对象
+   */
+  static class Handler {
+    private SocketChannel channel;
+    private ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+    private ByteBuffer writeBuffer = ByteBuffer.allocate(1024);
+    private Queue<String> writeQueue = new ArrayBlockingQueue(100);
+
+    public Handler(SocketChannel channel) {
+      this.channel = channel;
+    }
+
+    public void read(SelectionKey key) throws IOException {
+      // 假定每次读取消息都读取到了整包，直接清空 buffer
+      readBuffer.clear();
+      int readBytes = channel.read(readBuffer);
+      if (readBytes == -1) {
+        channel.close();
+        key.cancel();
+        System.out.println("客户端已关闭");
+        return;
+      }
+      // 上面写了 buffer，这里要读取的话，就需要翻转
+      readBuffer.flip();
+      byte[] bytes = new byte[readBuffer.remaining()];
+      readBuffer.get(bytes);
+      String body = new String(bytes, StandardCharsets.UTF_8);
+      Console.log("[{}]收到信息：{}", DateUtil.formatDateTime(new Date()), body);
+
+      // 收到一个消息，就发送一个当前时间
+      final String message = DateUtil.formatDateTime(new Date());
+      writeQueue.offer(message);
+    }
+
+    public void checkWrite() throws IOException {
+      final String msg = writeQueue.poll();
+      if (msg != null) {
+        writeBuffer.clear();
+        writeBuffer.put(msg.getBytes(StandardCharsets.UTF_8));
+        writeBuffer.flip();  // 我们写完之后翻转，使用 channel 的时候才能读取到数据
+        channel.write(writeBuffer);
+      }
+    }
+  }
 }
