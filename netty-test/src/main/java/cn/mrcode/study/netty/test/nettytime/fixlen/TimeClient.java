8ba4b17516d0fc1136bda8d3d176397c733041c2
zq99299 <99299684@qq.com>
2022-07-27 22:09:21 +0800

    消息定长代码演示

---
+++ netty-test/src/main/java/cn/mrcode/study/netty/test/nettytime/fixlen/TimeClient.java
@@ -0,0 +1,99 @@
+package cn.mrcode.study.netty.test.nettytime.fixlen;
+
+import cn.hutool.core.lang.Console;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.FixedLengthFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author mrcode
+ * @date 2022/7/19 22:12
+ */
+public class TimeClient {
+
+    public static void main(String[] args) throws InterruptedException {
+        int port = 8086;
+        new TimeClient().connect("127.0.0.1", port);
+    }
+
+    public void connect(String host, int port) throws InterruptedException {
+        // 配置客户端NIO线程组
+        NioEventLoopGroup group = new NioEventLoopGroup();
+        Bootstrap bootstrap = new Bootstrap();
+        try {
+
+            bootstrap.group(group)
+                    .channel(NioSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY, true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            // 通道建立后，绑定我们的处理类
+                            ch.pipeline()
+                                    // 由于服务端发送来的消息是固定长度的，所以我们需要使用 FixedLengthFrameDecoder 解码器来解码
+                                    // 服务端的消息是 yyyy-MM-dd HH:mm:ss 类型
+                                    .addLast(new FixedLengthFrameDecoder("yyyy-MM-dd HH:mm:ss".length()))
+                                    .addLast(new StringDecoder(StandardCharsets.UTF_8))
+                                    .addLast(new TimeClientHandler());
+                        }
+                    });
+            // 发起异步链接操作
+            ChannelFuture future = bootstrap.connect(host, port).sync();
+            // 同步阻塞，链路关闭才被唤醒
+            future.channel().closeFuture().sync();
+
+        } finally {
+            //优雅退出，释放NIO线程组
+            group.shutdownGracefully();
+        }
+    }
+
+    private class TimeClientHandler extends ChannelInboundHandlerAdapter {
+
+        /**
+         * 客户端和服务端 TCP 链路建立成功之后，该方法被调用
+         *
+         * @param ctx
+         * @throws Exception
+         */
+        @Override
+        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+            // 粘包测试，连续发送 100 条语句
+            byte[] req = ("12345").getBytes(StandardCharsets.UTF_8);
+            for (int i = 0; i < 100; i++) {
+                ByteBuf firstMessage = Unpooled.buffer(req.length);
+                firstMessage.writeBytes(req);
+                ctx.writeAndFlush(firstMessage);
+            }
+        }
+
+        /**
+         * 服务端返回应答消息时，该方法被调用
+         *
+         * @param ctx
+         * @param msg
+         * @throws Exception
+         */
+        @Override
+        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+            // 同样，由于使用了 StringDecoder 解码器，所以 msg 是一个 String 类型
+            String body = (String) msg;
+            Console.log("== 接收到消息:{}", body);
+            ctx.close();
+        }
+
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+            cause.printStackTrace();
+            ctx.close();
+        }
+    }
+}
---
+++ netty-test/src/main/java/cn/mrcode/study/netty/test/nettytime/fixlen/TimeServer.java
@@ -0,0 +1,100 @@
+package cn.mrcode.study.netty.test.nettytime.fixlen;
+
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.lang.Console;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.FixedLengthFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+/**
+ * @author mrcode
+ * @date 2022/7/19 22:05
+ */
+public class TimeServer {
+
+    public static void main(String[] args) throws InterruptedException {
+        int port = 8086;
+        new TimeServer().bind(port);
+    }
+
+    public void bind(int port) throws InterruptedException {
+        // 配置服务端的NIO线程组
+        // 包含一组NIO线程，专门用于网络事件的处理
+        // 实际上他们就是Reactor线程组。
+
+        //用于接收客户端的链接
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        // 用于SocketChannel的网络读写
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        // 引导配置
+        ServerBootstrap starp = new ServerBootstrap();
+        try {
+            starp.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class) // 指定通道类型
+                    .option(ChannelOption.SO_BACKLOG, 1024) // 缓存大小
+                    .childHandler(new ChildChannelHandler()); // 绑定处理器
+            // 绑定端口，同步等待成功。 ChannelFuture： 类似 Jdk.Future, 用于异步操作的通知回调
+            ChannelFuture channelFuture = starp.bind(port).sync();
+            // 等待服务器监听端口关闭。该方法会阻塞，链路关闭后，会被唤醒
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            //优雅退出，释放线程池资源
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    private class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
+
+        @Override
+        protected void initChannel(SocketChannel ch) throws Exception {
+            ch.pipeline()
+                    // 定长消息解码器，指定消息长度为 5 个 byte
+                    .addLast(new FixedLengthFrameDecoder(5))
+                    // 将 ByteBuf 中获取到的 byte 转换为 String
+                    .addLast(new StringDecoder(StandardCharsets.UTF_8))
+                    .addLast(new TimeServerHanler());
+        }
+    }
+
+
+    private class TimeServerHanler extends ChannelInboundHandlerAdapter {
+
+        @Override
+        public void channelRead(ChannelHandlerContext ctx, Object msg) {
+            // 需要注意的是：使用了 StringDecoder 之后，这里的消息就是 String 类型了
+            String body = (String) msg;
+            Console.log("== 请求消息：{}", body);
+
+            ByteBuf resp = Unpooled.copiedBuffer(DateUtil.formatDateTime(new Date()).getBytes(StandardCharsets.UTF_8));
+            ctx.write(resp); // 异步发送应答
+        }
+
+        @Override
+        public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+            // 将消息发送队列中的消息写入到 SocketChannel 中发送给对方
+            // 性能考虑，放置频繁唤醒 Selector 进行消息发送。
+            // Netty 的方法并不直接将消息写入 SocketChannel 中
+            // 调用 write 只是把消息放到了发送缓冲数组中。
+            // 通过 flush 方法将缓冲区中的消息全部写入到 SocketChannel 中
+            ctx.flush();
+        }
+
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+            // 发生异常，释放相关句柄资源
+            ctx.close();
+            cause.printStackTrace();
+        }
+    }
+
+}
